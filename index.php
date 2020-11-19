d46e28933c071b4097fc077687b2a5bb9baed3f7
unknown <careermiqdad@gmail.com>
2020-11-19 09:59:14 +0700

    tambah calculator

--- index.php
+++ index.php
@@ -491,6 +491,24 @@
                         <p><strong style="text-align:center">AOI Monitoring </strong> is Web App for Inspecting Machine Parameters in realtime and give notification use telegram bot <strong>Technology use Codeigniter 3</strong></p>
                     </div> <!-- end of col -->
                 </div> <!-- end of row -->
+                <br><br> <!-- end of row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!-- Video Preview -->
+                        <div class="image-container">
+                            <div class="video-wrapper">
+                                <a class="popup-youtube" href="https://www.youtube.com/watch?v=FN4bOJSMsmo" data-effect="fadeIn">
+                                    <img class="img-fluid" src="images/calcu.png" alt="alternative">
+                                    <span class="video-play-button">
+                                        <span></span>
+                                    </span>
+                                </a>
+                            </div> <!-- end of video-wrapper -->
+                        </div> <!-- end of image-container -->
+                        <!-- end of video preview -->
+                        <p><strong style="text-align:center">Simple Calculator</strong> is Mobile App for calculating numbers <strong>Technology use Flutter</strong></p>
+                    </div> <!-- end of col -->
+                </div> <!-- end of row -->
             </div> <!-- end of container -->
         </div> <!-- end of basic-3 -->
         <!-- end of video -->
